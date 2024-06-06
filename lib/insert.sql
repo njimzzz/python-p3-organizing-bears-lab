33d99be82d7b99d0b31bf9e677d805b88a553cd0
Kyle <kylejrgurlad@gmail.com>
2024-06-06 18:39:39 +0300

    initial commit

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 5, 'M', 'Black', 'choleric', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 3, 'M', 'Brown', 'phlegmatic', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 2, 'F', 'White', 'melancholic', 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergent Brown', 8, 'M', 'Brown', 'sanguine', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 4, 'F', 'Brown', 'melancholic', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 3, 'M', 'Black', 'choleric', 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 5, 'F', 'White', 'sanguine', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ( NULL, 8, 'F', 'White', 'phlegmatic', 0);
+
